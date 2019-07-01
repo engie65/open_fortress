ab318f411270f22012924a5b4a952a1c4661cf0f
ficool2 <34815548+ficool2@users.noreply.github.com>
2019-07-01 21:04:11 +0100

    Fixed crashing

---
+++ bin/move.bat
@@ -0,0 +1,9 @@
+del client.dll
+del server.dll
+del client.pdb
+del server.pdb
+
+move "E:\Program Files (x86)\Steam\steamapps\sourcemods\Open-Fortress-Source\game\client\Release_open_fortress\client.dll" "E:\Program Files (x86)\Steam\steamapps\sourcemods\open_fortress\bin"
+move "E:\Program Files (x86)\Steam\steamapps\sourcemods\Open-Fortress-Source\game\client\Release_open_fortress\client.pdb" "E:\Program Files (x86)\Steam\steamapps\sourcemods\open_fortress\bin"
+move "E:\Program Files (x86)\Steam\steamapps\sourcemods\Open-Fortress-Source\game\server\Release_open_fortress\server.dll" "E:\Program Files (x86)\Steam\steamapps\sourcemods\open_fortress\bin"
+move "E:\Program Files (x86)\Steam\steamapps\sourcemods\Open-Fortress-Source\game\server\Release_open_fortress\server.pdb" "E:\Program Files (x86)\Steam\steamapps\sourcemods\open_fortress\bin"
